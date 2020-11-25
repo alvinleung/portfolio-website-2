4acc8152fea18a4223ffa7f0258010cbca4028cb
Alvin Leung <alvinleung2009@gmail.com>
2020-11-25 05:30:03 -0800

    Removed Junk

--- src/components/Title.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Title: React.FC = () => {
-  return <h1>Hi, this is Alvin. I fight for my users against bad designs by combining function with aesthetics.</h1>;
-}
-
-export default Title;
