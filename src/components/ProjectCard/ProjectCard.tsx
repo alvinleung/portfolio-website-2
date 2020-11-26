84faf62f05c76ad61dbc3ed5e1ad7e80f01c5f99
Alvin Leung <alvinleung2009@gmail.com>
2020-11-26 06:06:05 -0800

    Removed disrupting AnimatePresence in Project Card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -54,46 +54,44 @@ export const ProjectCard: React.FC<Props> = (props: Props) => {
   const [isViewing, setIsViewing] = useState(false);
 
   const cardContent = (
-    <AnimatePresence>
-      <motion.div
-        variants={isViewing ? variantsSelectedProject : variantsDefault}
-        initial="inital"
-        animate="enter"
-        exit="exit"
-        // layout
-        // layoutId={props.slug}
+    <motion.div
+      variants={isViewing ? variantsSelectedProject : variantsDefault}
+      initial="inital"
+      animate="enter"
+      exit="exit"
+      // layout
+      // layoutId={props.slug}
+    >
+      <ReactiveCard
+        reactive={!props.isViewing}
+        style={{
+          backgroundImage: `url(${props.cover})`,
+          backgroundSize: 'cover',
+        }}
       >
-        <ReactiveCard
-          reactive={!props.isViewing}
-          style={{
-            backgroundImage: `url(${props.cover})`,
-            backgroundSize: 'cover',
-          }}
+        <motion.div
+          className={style.projectType}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: props.isViewing ? 0 : 1 }}
         >
-          <motion.div
-            className={style.projectType}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: props.isViewing ? 0 : 1 }}
-          >
-            {/* UX/UI Design */}
-            {props.catagory}
-          </motion.div>
-          <motion.h3
-            initial={{ opacity: 0 }}
-            animate={{ opacity: props.isViewing ? 0 : 1 }}
-          >
-            {props.title}
-          </motion.h3>
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: props.isViewing ? 0 : 1 }}
-          >
-            {/* Build connections in the community one task at a time. */}
-            {props.tagline}
-          </motion.p>
-        </ReactiveCard>
-      </motion.div>
-    </AnimatePresence>
+          {/* UX/UI Design */}
+          {props.catagory}
+        </motion.div>
+        <motion.h3
+          initial={{ opacity: 0 }}
+          animate={{ opacity: props.isViewing ? 0 : 1 }}
+        >
+          {props.title}
+        </motion.h3>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: props.isViewing ? 0 : 1 }}
+        >
+          {/* Build connections in the community one task at a time. */}
+          {props.tagline}
+        </motion.p>
+      </ReactiveCard>
+    </motion.div>
   );
 
   return (
