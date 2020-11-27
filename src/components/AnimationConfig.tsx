4a0207cf8d1c6c03b029ed1ff14e2520db5b1484
Alvin Leung <alvinleung2009@gmail.com>
2020-11-27 05:18:37 -0800

    Setup Animation Config file

---
+++ src/components/AnimationConfig.tsx
@@ -0,0 +1,11 @@
+export const ANIMIATION_FAST = 0.2;
+export const ANIMIATION_NORMAL = 0.5;
+export const ANIMIATION_EASING = [0.16, 1, 0.3, 1];
+
+export const AnimationConfig = {
+  VERY_FAST: 0.1,
+  FAST: 0.2,
+  NORMAL: 0.5,
+  EASING: [0.16, 1, 0.3, 1], // ease out expo
+  // EASING: [0.22, 1, 0.36, 1], // ease out quint
+};
