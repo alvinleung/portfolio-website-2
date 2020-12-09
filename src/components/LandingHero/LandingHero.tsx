efe90700f91f4a98c8844757c815d31ae4d531e7
Alvin Leung <alvinleung2009@gmail.com>
2020-12-09 01:34:56 -0800

    Responsive hero scroll progress

--- src/components/LandingHero/LandingHero.tsx
+++ src/components/LandingHero/LandingHero.tsx
@@ -1,12 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion, useViewportScroll, useTransform } from 'framer-motion';
 import './LandingHero.scss';
 import { AnimationConfig, AnimationVariants } from '../AnimationConfig';
 
 export default () => {
   const { scrollY } = useViewportScroll();
-  const headerOpacity = useTransform(scrollY, [0, 400], [1, 0]);
-  const headerScale = useTransform(scrollY, [0, 400], [1, 0.95]);
+
+  const [windowHeight, setWindowHeight] = useState(0);
+  const fullyHiddenPos = windowHeight * 0.37; // determine at which scroll position should it be hidden
+
+  const headerOpacity = useTransform(scrollY, [0, fullyHiddenPos], [1, 0]);
+  const headerScale = useTransform(scrollY, [0, fullyHiddenPos], [1, 0.95]);
+
+  useEffect(() => {
+    // for SSR support
+    if (typeof window === 'undefined') return;
+
+    const updateWindowHeight = () => {
+      setWindowHeight(window.innerHeight);
+    };
+    window.addEventListener('resize', updateWindowHeight);
+    updateWindowHeight();
+    return () => {
+      window.addEventListener('resize', updateWindowHeight);
+    };
+  }, []);
 
   return (
     <div className="LandingHero">
