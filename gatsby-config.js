7167ecb6171db9585fd5d97c31f6393480702702
Alvin Leung <alvinleung2009@gmail.com>
2021-03-09 14:15:50 -0800

    Remove google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,34 +84,5 @@ module.exports = {
         },
       },
     },
-    // All other plugins
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-BSRGTE944J", // Google Analytics / GA
-          // optional
-          // "OPTIONAL----AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "OPTIONAL----DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          // optimize_id: "G-BSRGTE944J",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
-      },
-    },
   ],
 };
