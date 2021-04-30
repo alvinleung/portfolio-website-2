e2496c193721d792ec6a5125265cd05a7ed67756
Alvin Leung <alvinleung2009@gmail.com>
2021-04-30 16:50:28 -0700

    testing

---
+++ src/pages/new.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+interface Props {}
+
+export default function newPage({}: Props): ReactElement {
+  return (
+    <main id="about" className="full-width nav-padding main-grid">
+      <p>fdsafdsfa</p>
+    </main>
+  );
+}
